9615ea3bd900fe428e910f6f96415562d161fe4e
Pedro Mass <pedro.j.mass@gmail.com>
2014-11-07 19:39:28 -0500

    Finish section 6.2.3

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
-	validates	:name, presence: true
-	validates	:email, presence: true
+	validates	:name, 
+					presence: true,
+					length: { maximum: 50 }
+	validates	:email, 
+					presence: true
 end
